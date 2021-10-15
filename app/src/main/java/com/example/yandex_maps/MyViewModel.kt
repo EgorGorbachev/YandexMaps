c1611f2d861b62e3d43158e18646436da9c0726d
Egor Gorbachev <e.gorbachev.vironit@vironit.ru>
2021-10-15 12:31:28 +0300

    feat: add fun in viewModel

--- app/src/main/java/com/example/yandex_maps/MyViewModel.kt
+++ app/src/main/java/com/example/yandex_maps/MyViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.location.Geocoder
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.lifecycle.AndroidViewModel
@@ -13,23 +14,25 @@ import java.util.HashSet
 
 class MyViewModel(
 	application: Application
-): AndroidViewModel(application) {
-
-	var startLat:Double? = null
-	var startLng:Double? = null
-	var endLat:Double? = null
-	var endLng:Double? = null
+) : AndroidViewModel(application) {
+	
+	var startLat: Double? = null
+	var startLng: Double? = null
+	var endLat: Double? = null
+	var endLng: Double? = null
 	
 	var mark: PlacemarkMapObject? = null
 	
 	
-	fun getAddressLine(lat: Double, lng: Double, geocoder:Geocoder): String {
+	fun getAddressLine(lat: Double, lng: Double, geocoder: Geocoder): String {
 		val address = geocoder.getFromLocation(lat, lng, 1)
 		return if (address[0] != null) {
 			address[0].getAddressLine(0).toString()
 		} else {
 			"This place have incorrect address"
 		}
+		
+		
 	}
 	
 	fun getVehicleType(transport: Transport, knownVehicleTypes: HashSet<String>): String? {
@@ -41,3 +44,5 @@ class MyViewModel(
 		return null
 	}
 }
+
+class C (val map:MutableMap<String, out String>)
